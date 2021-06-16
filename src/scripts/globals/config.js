5c1ae8423e5a2a56c54f511b9fa9163e38f37f10
Erdeprof <rizkidarmawanrpl@gmail.com>
2021-06-16 10:43:59 +0700

    Commit app shell done

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '1829d3eb6e361af89329b8fc9b669dfe',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
